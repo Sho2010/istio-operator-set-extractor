ea324fb3bff4a86380d2d7353cb29b751e0b324d
sho naito <sho20100@gmail.com>
2022-10-06 13:07:54 +0000

    Add go-diff

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/sergi/go-diff/diffmatchpatch"
+)
+
+const (
+	text1 = "Lorem dolor."
+	text2 = "Lorem dolor sit amet."
+)
+
 func main() {
-	println("Hello, World!")
+	dmp := diffmatchpatch.New()
+
+	diffs := dmp.DiffMain(text1, text2, false)
+
+	fmt.Println(dmp.DiffPrettyText(diffs))
 }
